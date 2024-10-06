462c1064eab61a019ede40854f41387c8b77e073
Rory Pinkney <rorypinkney@yahoo.co.uk>
2024-10-06 01:33:58 +0100

    rewrote AllocationNode logic to support array declaration

--- src/constants/allocation.ts
+++ src/constants/allocation.ts
@@ -9,3 +9,15 @@ export const dataTypeMap: Record<string, DataType> = {
   '.asciz': DataType.BYTE,
   '.skip': DataType.BYTE
 }
+
+export const dataTypeBitSizeMap = {
+  [DataType.BYTE]: 8,
+  [DataType.HWORD]: 16,
+  [DataType.WORD]: 32
+} as const;
+
+export const dataTypeBufferConstructorMap = {
+  [DataType.BYTE]: Uint8Array,
+  [DataType.HWORD]: Uint16Array,
+  [DataType.WORD]: Uint32Array
+} as const;
--- src/syntax/AllocationNode.ts
+++ src/syntax/AllocationNode.ts
@@ -1,5 +1,5 @@
 import { bitset } from "@/assets/bitset";
-import { DataType, dataTypeMap } from '@/constants';
+import { DataType, dataTypeBufferConstructorMap, dataTypeMap, dataTypeBitSizeMap } from '@/constants';
 import { Token } from "prismjs";
 import { SyntaxError } from "../interpreter/error";
 import { SyntaxNode } from "./SyntaxNode";
@@ -7,37 +7,35 @@ import { tokens } from "@/constants/tokens";
 
 export class AllocationNode extends SyntaxNode {
   protected _identifier: string;
-  protected _data: Uint8Array;
+  protected _data: ArrayBuffer;
 
   protected _type: DataType;
   private _specificType!: string;
 
   get identifier() : string { return this._identifier; }
-  get data() : Uint8Array { return this._data; }
+  get data() : Uint8Array { return new Uint8Array(this._data); }
   get type() : string { return this._specificType; }
 
   constructor(statement: Token[], lineNumber: number, currentToken: number = 0) {
     super(statement, lineNumber, currentToken);
 
     this._identifier = (this.nextToken().content as string).slice(0, -1);
-    this._type = dataTypeMap[this.nextToken().content as string];
-    this._data = this.parseData(this.peekToken());
+    this._type = dataTypeMap[this.peekToken().content as string];
+    this._specificType = this.nextToken().content as string;
 
-    this.nextToken();
+    this._data = this.parseData();
 
     if (this.hasToken()) throw new SyntaxError(`Unexpected token '${this.peekToken().content}' after valid data declaration end.`, statement, lineNumber, currentToken);
   }
 
-  parseData(token: Token) : Uint8Array {
-    this._specificType = this.previousToken().content as string;
+  parseData() : ArrayBuffer {
+    if (this._specificType === ".asciz") return this.parseString(this.nextToken());
+    if (this._specificType === ".byte") return this.parseIntArray(DataType.BYTE);
+    if (this._specificType === ".hword") return this.parseIntArray(DataType.HWORD);
+    if (this._specificType === ".word") return this.parseIntArray(DataType.WORD);
+    if (this._specificType === ".skip") return this.parseSkip(this.nextToken());
 
-    if (this.previousToken().content === ".asciz") return this.parseString(token);
-    if (this.previousToken().content === ".byte") return this.parseByte(token);
-    if (this.previousToken().content === ".hword") return this.parseHWord(token);
-    if (this.previousToken().content === ".word") return this.parseWord(token);
-    if (this.previousToken().content === ".skip") return this.parseSkip(token);
-
-    throw new SyntaxError(`Unrecognised data type '${this.previousToken().content}'.`, this.statement, this.lineNumber, this._currentToken - 1)
+    throw new SyntaxError(`Unrecognised data type '${this._specificType}'.`, this.statement, this.lineNumber, this._currentToken - 1)
   }
 
   parseString(token: Token) : Uint8Array {
@@ -45,7 +43,7 @@ export class AllocationNode extends SyntaxNode {
       throw SyntaxError.badToken(tokens.string, token, this._statement, this._lineNumber, this._currentToken);
     }
 
-    const string = (token.content as string).slice(1, -1);
+    const string = (token.content as string).slice(1, -1);    // slice off quotes
     const withEscapes = JSON.parse(`"${string}"`);
     const data = new Uint8Array(new ArrayBuffer(withEscapes.length + 1));
 
@@ -54,30 +52,20 @@ export class AllocationNode extends SyntaxNode {
     return data;
   }
 
-  parseByte(token: Token) : Uint8Array {
-    const data = new Uint8Array(new ArrayBuffer(1));
-    const bits = bitset(8, this.parseNum(token));
-    data[0] = parseInt(bits.join(''), 2);
+  parseIntArray(size: DataType) : ArrayBuffer {
+    const ints: number[] = [this.parseSizedInt(this.nextToken(), size)];
 
-    return data;
+    while (this.hasToken()) {
+      this.parseComma(this.nextToken());
+      ints.push(this.parseSizedInt(this.nextToken(), size));
+    }
+
+    return new dataTypeBufferConstructorMap[size](ints);
   }
 
-  parseHWord(token: Token) : Uint8Array {
-    const buffer = new ArrayBuffer(2);
-    const data = new Uint16Array(buffer);
-    const bits = bitset(16, this.parseNum(token));
-    data[0] = parseInt(bits.join(''), 2);
-
-    return new Uint8Array(buffer);
-  }
-
-  parseWord(token: Token) : Uint8Array {
-    const buffer = new ArrayBuffer(4);
-    const data = new Uint32Array(buffer);
-    const bits = bitset(32, this.parseNum(token));
-    data[0] = parseInt(bits.join(''), 2);
-
-    return new Uint8Array(buffer);
+  parseSizedInt(token: Token, size: DataType) : number {
+    const bits = bitset(dataTypeBitSizeMap[size], this.parseNum(token));
+    return parseInt(bits.join(''), 2);
   }
 
   parseSkip(token: Token) : Uint8Array {
